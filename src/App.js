277d89aa7dc218eb23766faf86c044ad78be7cfa
pornrapat309 <pornrapat.kaew@gmail.com>
2023-08-29 16:15:40 +0700

    finish: step-5 header-customize

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.scss';
+import {FaHome} from 'react-icons/fa';
 
 function App() {
   return (
     <div className='todo'>
-        <div className="todo__header">Header</div>
+        <div className="todo__header">
+          <header className='header'>
+            <div className='header__logo'>
+              <FaHome />
+            </div>
+            <div className='header__text'>
+              <h1>Todolist</h1>
+              </div>
+            <div className='header__search'>
+              <input />
+            </div>
+          </header>
+        </div>
         <div className="todo__sidebar">SideBar</div>
         <div classNAme="todo__content">TodoContent</div>
     </div>
